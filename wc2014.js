7da8e8be4becb1d047666c51da435e6179ebab74
Geoff Unger <geoff@shopigniter.com>
2014-06-19 16:33:10 -0700

    Added initial files, basic reporter working

---
+++ wc2014.js
@@ -0,0 +1,57 @@
+var http = require('http');
+
+var urlToFetch = 'http://worldcup.sfg.io/matches';
+
+var usage = function () {
+    console.log("Usage: node scratch 'team name' - (if the team name is two words, use quotes)");
+}
+
+var team = process.argv[2];
+
+team || usage();
+
+var getGames = function () {
+
+    var games = {};
+
+    process.stdout.write("Fetching data...");
+
+    http.get(urlToFetch, function (res) {
+        var jsonGames = '';
+
+        res.on('data', function (chunk) {
+            jsonGames += chunk.toString();
+            process.stdout.write(".");
+        });
+
+        res.on('end', function () {
+            games = JSON.parse(jsonGames);
+            process.stdout.write("\n");
+            displayGames(games);
+        })
+
+        res.on('error', function (err) {
+            console.error(err);
+        });
+
+    });
+
+}
+
+var displayGames = function(games){
+    games.map(function(game){
+
+            if((game.home_team.country == team) || (game.away_team.country == team)){
+                game.winner && console.log("Winner: " + game.winner);
+            }
+
+    })
+}
+
+if (team) {
+    getGames();
+}
+
+
+
+
